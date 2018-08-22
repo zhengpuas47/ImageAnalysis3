138d6d7e7ed0068cb6375308023bd3240ae7970b
zhengpuas47 <zhengpuas47@gmail.com>
2018-08-22 12:32:03 -0400

    updated examples and startup file

---
+++ required_files/Startup_py3.py
@@ -0,0 +1,16 @@
+import sys, os, glob, time
+import numpy as np
+import scipy
+import sklearn 
+from PIL import Image
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import pickle
+from IPython.display import clear_output
+
+# reload
+from importlib import reload
+# add Document to path
+sys.path.append(r'C:\Users\puzheng\Documents')
+# image analysis functions for python3
+import ImageAnalysis3 as ia
