9ae8d7f7a876c7e3853577770ddf6410f3a64d7c
zhengpuas47 <zhengpuas47@gmail.com>
2019-01-23 18:04:14 +0800

    change crop_limits input

--- corrections.py
+++ corrections.py
@@ -1639,9 +1639,8 @@ def fast_illumination_correction(im, correction_channel, single_im_size=_image_s
             _im = im.copy()
         # case where you need to crop image
         else:
-            if crop_limits is None: # if crop-limits is not given
-                _im = im.copy()
-            elif len(crop_limits) == 2:
+            print(crop_limits)
+            if len(crop_limits) == 2:
                 _im = im.copy()[:, crop_limits[0][0]:crop_limits[0]
                                 [1], crop_limits[1][0]:crop_limits[1][1]]
             else:
