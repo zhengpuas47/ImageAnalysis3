d186c70e988ce6eb4c3e567a9980f9625cf0efbe
zhengpuas47 <zhengpuas47@gmail.com>
2019-07-04 15:03:33 -0400

    bug fixed in gene-spot finding

--- classes.py
+++ classes.py
@@ -2890,7 +2890,8 @@ class Cell_Data():
         """
         # check input
         _data_type=str(_data_type).lower()
-        if _data_type not in ['all', 'cell_info', 'unique', 'rna-unique' ,'combo', 'decoded','distance_map']:
+        if _data_type not in ['all', 'cell_info', 'distance_map'] \ 
+                and _data_type not in self.shared_parameters['allowed_data_types']:
             raise ValueError(f"Wrong _data_type kwd ({_data_type}) given!")
         if not _save_folder and not hasattr(self, 'save_folder'):
             raise ValueError('Save folder info not given!')
